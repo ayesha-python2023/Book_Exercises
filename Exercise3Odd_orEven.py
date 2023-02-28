958e7633542b572b67343aa3e9bf11cde0b62969
ayesha-python2023 <ayesha@protonmail.ch>
2023-02-28 14:08:08 -0700

    odd and even numbers

--- Exercise3Odd_orEven.py
+++ Exercise3Odd_orEven.py
@@ -1,1 +1,8 @@
 #how to find odd or even in python
+
+number = int(input("Please enter a number: "))
+
+if (number % 2) == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
